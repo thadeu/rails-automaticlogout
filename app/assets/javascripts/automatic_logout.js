e67945ec46b2b3cbeedde88e4da74b8214542116
Thadeu Esteves Jr <tadeuu@gmail.com>
2016-12-09 15:09:45 -0300

    Fix: === instead ==

--- app/assets/javascripts/automatic_logout.js
+++ app/assets/javascripts/automatic_logout.js
@@ -3,13 +3,13 @@
     $.ajax({
       url: '/status_automatic_logout',
       success: function(data) {
-        if (data.timeout != "null" && data.live == true){
+        if (data.timeout !== "null" && data.live === true){
           var response_timeout = (new Date(data.timeout).getTime());
 
           setInterval(function(){
             var now_time = (new Date().getTime());
 
-            if ((response_timeout < now_time) == true){
+            if ((response_timeout < now_time) === true){
               if (confirm(data.message)){
                 window.location.href = '/destroy_automatic_logout';
               }
